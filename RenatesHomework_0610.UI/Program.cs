d33e70a5a20caaf30647462e4e675dc4b5c18615
rreen <rreen@LAPTOP-F3JFDV36>
2021-10-07 20:44:20 +0300

    Add project files.

---
+++ RenatesHomework_0610.UI/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RenatesHomework_0610.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is what I ate today:");
+            Console.Write("3 apples, ");
+            Console.Write("2 bananas");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("This is what I didn't eat:");
+            Console.Write("Mozarella sticks, ");
+            Console.Write("Candy");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+         
+        }
+    }
+}
