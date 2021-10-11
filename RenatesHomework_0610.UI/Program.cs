e189f1ebbb8278c8b2d2f84fe45a88dc539d6983
Renate Slucka <rreennaattee@gmail.com>
2021-10-11 21:46:31 +0300

    11 oct

--- RenatesHomework_0610.UI/Program.cs
+++ RenatesHomework_0610.UI/Program.cs
@@ -10,6 +10,7 @@ namespace RenatesHomework_0610.UI
             Console.Write("3 apples, ");
             Console.Write("2 bananas");
             Console.WriteLine();
+            #region Testabc
             Console.WriteLine();
             Console.WriteLine("This is what I didn't eat:");
             Console.Write("Mozarella sticks, ");
@@ -17,9 +18,20 @@ namespace RenatesHomework_0610.UI
             Console.WriteLine();
             Console.WriteLine("123123123");
             Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine();
+            #endregion
+
+
+            Console.WriteLine("ABC " + "DEF");
+            Console.WriteLine($"ABC DEF");
             Console.ReadLine();
+
+            int number1 = 1;
+            int number2 = 3;
+            int result = number1 + number2;
+
+
+            Console.WriteLine($"number 1 number 2");
+
 
             //comment
             //comment
